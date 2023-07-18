3e5e4f47abfea9c9b82523d682711f8571bc7259
bigspambot <67501435+bigspambot@users.noreply.github.com>
2023-07-18 12:18:49 +0100

    start 6 - not finished yet

---
+++ src/components/CommentCard.jsx
@@ -0,0 +1,14 @@
+const CommentCard = () => {
+    return (
+        <>
+            <p>Comment ID</p>
+            <p>Votes</p>
+            <p>Created At</p>
+            <p>Author</p>
+            <p>Body</p>
+            <p>Article ID</p>
+        </>
+    )
+}
+
+export default CommentCard
---
+++ src/components/CommentsList.jsx
@@ -0,0 +1,16 @@
+import CommentCard from "./CommentCard"
+
+const CommentsList = () => {
+    return (
+        <>
+            <CommentCard />
+            <CommentCard />
+            <CommentCard />
+            <CommentCard />
+            <CommentCard />
+            <CommentCard />
+        </>
+    )
+}
+
+export default CommentsList
