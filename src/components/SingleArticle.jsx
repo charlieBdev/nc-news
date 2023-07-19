221f2d0d6b534eabe465e6624b059cb0bcbbebea
bigspambot <67501435+bigspambot@users.noreply.github.com>
2023-07-19 11:43:19 +0100

    sorry have to fix like button here

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -19,53 +19,57 @@ const SingleArticle = (props) => {
     } = props.article
 
     const [userVotes, setUserVotes] = useState(0)
-    const [isLiked, setIsLiked] = useState(false)
+    const [isClicked, setIsClicked] = useState(false)
 
     const handleLike = (event) => {
         event.preventDefault()
         setUserVotes((currVotes) => {
-            if (!isLiked) {
+            if (!isClicked) {
                 return currVotes + 1
             } else {
                 return currVotes - 1
             }
-            
         })
-        if (!isLiked) {
-            setIsLiked(true)
+        if (!isClicked) {
             patchArticleVotes(article_id, 1)
-            .catch(() => {
-                setUserVotes(0)
-                setIsLiked(false)
+            .then(() => {
+                setIsClicked(true)
+            })
+            .catch((err) => {
+                setUserVotes((currentUserVotes) => {
+                    return currentUserVotes - 1
+                })
+                setIsClicked(false)
             })
         } else {
-            setIsLiked(false)
             patchArticleVotes(article_id, -1)
-            .catch(() => {
-                setUserVotes(1)
-                setIsLiked(true)
+            .then(() => {
+                setIsClicked(false)
+            })
+            .catch((err) => {
+                setUserVotes((currentUserVotes) => {
+                    return currentUserVotes + 1
+                })
+                setIsClicked(true)
             })
         }
     }
 
     return (
-        <>
-            <article className="single-article-card">
-                <h3>{title}</h3>
-                <img className="article-img" src={article_img_url} alt={title} />
-                <h4><span className="by-author">By</span> {author}</h4>
-                <p className="article-body">{body}</p>
-                <div className="article-info">
-                    <p>Article ID: {article_id}</p>
-                    <p>Topic: {topic}</p>
-                    <p>Created: {formatDate(created_at)}</p>
-                </div>
-                <p>{votes + userVotes} votes</p>
-                    <button className={!isLiked ? "like-btn" : "liked-btn"} onClick={handleLike}>👍</button>
-                <Link to="/">Back</Link>
-            </article>
-            <CommentsList article_id={article_id}/>
-        </>
+        <article className="single-article-card">
+            <h3>{title}</h3>
+            <img className="article-img" src={article_img_url} alt={title} />
+            <h4><span className="by-author">By</span> {author}</h4>
+            <p className="article-body">{body}</p>
+            <div className="article-info">
+                <p>Article ID: {article_id}</p>
+                <p>Topic: {topic}</p>
+                <p>Created: {formatDate(created_at)}</p>
+            </div>
+            <p>{votes + userVotes} votes</p>
+                <button className={!isClicked ? "like-btn" : "liked-btn"} onClick={handleLike}>👍</button>
+            <Link to="/">Back</Link>
+        </article>
     )
 }
 
