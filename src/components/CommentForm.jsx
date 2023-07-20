6d5f0ec6242680a1bee833ca7701e492d268a70e
bigspambot <67501435+bigspambot@users.noreply.github.com>
2023-07-20 14:53:04 +0100

    fix input length and disabled button until post complete

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -8,12 +8,15 @@ const CommentForm = (props) => {
     const { user, setComments } = props
     const [newComment, setNewComment] = useState('')
     const [apiError, setApiError] = useState(false)
+    const [btnIsDisabled, setBtnIsDisabled] = useState(false)
 
     const handleSumbit = (event) => {
         event.preventDefault()
-        if (newComment.length <= 50) {
+        if (newComment.length > 0 && newComment.length <= 200) {
+            setBtnIsDisabled(true)
             postComment(article_id, user, newComment)
             .then((res) => {
+                setBtnIsDisabled(false)
                 setComments((currComments) => {
                     return [res, ...currComments]
             })
@@ -29,8 +32,8 @@ const CommentForm = (props) => {
     return (
         <form onSubmit={handleSumbit}>
             <textarea rows="3" cols="25" type="text" placeholder="Add comment" value={newComment} onChange={(event) => setNewComment(event.target.value)}/>
-            <p>{50 - newComment.length} chars left</p>
-            <button disabled={newComment.length > 50 || newComment.length < 1} type="submit" value="submit">Comment</button>
+            <p>{200 - newComment.length} chars left</p>
+            <button disabled={btnIsDisabled} type="submit" value="submit">Comment</button>
             {apiError && <p>Sorry, that did not work. Please try again.</p>}
         </form>
     )
