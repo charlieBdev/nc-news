b596f06f5cd385a1bfb84626593de24ebdc07d22
bigspambot <67501435+bigspambot@users.noreply.github.com>
2023-07-21 11:00:11 +0100

    remove comments

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { useParams, useSearchParams } from "react-router-dom"
+import { useSearchParams } from "react-router-dom"
 import { getArticles } from "../utils/api"
 import ArticleCard from "./ArticleCard"
 import SortOptions from "./SortOptions"
@@ -7,8 +7,6 @@ import Navbar from "./Navbar"
 
 const ArticlesList = () => {
 
-    // const { topic } = useParams()
-    // console.log(topic, '<<< topic')
     const [searchParams, setSearchParams] = useSearchParams()
     const topicQuery = searchParams.get("topic")
     const sortByQuery = searchParams.get("sort_by")
@@ -45,34 +43,6 @@ const ArticlesList = () => {
         .catch((err) => {
             setIsError(true)
         })
-        // if (!topicQuery) {
-        //     getArticles()
-        //     .then((articles) => {
-        //         setArticles(articles)
-        //         setIsLoading(false)
-        //     })
-        //     .catch(() => {
-        //         setIsError(true)
-        //     })
-        // } else if (topicQuery) {
-        //     getArticlesByTopic(topicQuery)
-        //     .then((articles) => {
-        //         setArticles(articles)
-        //         setIsLoading(false)
-        //     })
-        //     .catch(() => {
-        //         setIsError(true)
-        //     })
-        // } else {
-        //     getArticlesByTopic(topicQuery, sortByQuery, orderQuery)
-        //     .then((filteredArticles) => {
-        //         setArticles(filteredArticles)
-        //         setIsLoading(false)
-        //     })
-        //     .catch(() => {
-        //         setIsError(true)
-        //     })
-        // }
     }, [topicQuery, sortByQuery, orderQuery])
 
     if (isLoading) {
